b07bbea0f5a91c1f620cf54956b8e88448142445
Tavion Harris <datboicoop97@gmail.com>
2025-10-20 03:55:11 -0400

    minor change

--- scripts/index.js
+++ scripts/index.js
@@ -34,11 +34,11 @@ const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-editProfileBtn.addEventListener("click", () => {
+editProfileBtn.addEventListener("click", function ()  {
   editProfileModal.classList.add("modal_is-opened");
 });
 
-editProfileCloseBtn.addEventListener("click", () => {
+editProfileCloseBtn.addEventListener("click", function ()  {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
@@ -46,11 +46,11 @@ const newPostBtn = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-newPostBtn.addEventListener("click", () => {
+newPostBtn.addEventListener("click", function ()  {
   newPostModal.classList.add("modal_is-opened");
 });
 
-newPostCloseBtn.addEventListener("click", () => {
+newPostCloseBtn.addEventListener("click", function ()  {
   newPostModal.classList.remove("modal_is-opened");
 });
 

cf06801cfd0d841d8cf9f282b2b5cbf043490748
Tavion Harris <datboicoop97@gmail.com>
2025-10-20 02:57:28 -0400

    spint 5 half done

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,36 @@
+const initialCards = [
+  {
+   Name: "Val Thorens",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+
+  {
+   Name: "Restaurant terrace",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+
+  {
+   Name: "An outdoor cafe",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+
+  {
+   Name: "A very long bridge, over the forest and through the trees",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+
+  {
+   Name: "Tunnel with morning light",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+
+  {
+   Name: "Mountain house",
+   Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
+
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -20,4 +53,9 @@ newPostBtn.addEventListener("click", () => {
 
 newPostCloseBtn.addEventListener("click", () => {
   newPostModal.classList.remove("modal_is-opened");
+});
+
+initialCards.forEach(function (card) {
+console.log (card.name);
+console.log (card.link);
 });

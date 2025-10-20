fe6e45264e17f3bc81552620cd2a534fe319ab74
Tavion Harris <datboicoop97@gmail.com>
2025-10-20 03:43:42 -0400

    sprint 5 real fix

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const initialCards = [
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
-  
+
   {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
